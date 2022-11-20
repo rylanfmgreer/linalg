625df4292807f59af3d9259a62a251d66e497b6e
Rylan Greer <rylangreer@MacBook-Pro.local>
2022-11-20 17:02:08 +0000

    adding in functions on doublevec

---
+++ src/doublevec_src/functions_on_doublevecs.cpp
@@ -0,0 +1,34 @@
+#include "functions_on_doublevecs.hpp"
+#include <float.h>
+namespace f_dv
+{
+    double mean(const DoubleVec& v)
+    {
+        return v.mean()
+    }
+
+    double max(const DoubleVec& v)
+    {
+        double mx = -1.0 * DBL_MAX;
+        std::for_each(v.data.begin(), v.data.end(),
+                      [&mx](double x)
+                      {
+                        if(x > mx)
+                            mx = x;
+                      });
+        return mx;
+    }
+
+    double min(const DoubleVec& v)
+    {
+        double mn = DBL_MAX;
+        std::for_each(v.data.begin(), v.data.end(),
+                      [&mn](double x)
+                      {
+                        if(x < mn)
+                            mn = x;
+                      });
+        return mn;
+    }
+
+};
---
+++ src/doublevec_src/functions_on_doublevecs.hpp
@@ -0,0 +1,7 @@
+#include "doublevec.hpp"
+namespace f_dv
+{
+    double mean(const DoubleVec& v);
+    double max(const DoubleVec& v);
+    double min(const DoubleVec& v);
+};
