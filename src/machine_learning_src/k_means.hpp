0be4056e93e5513cd37b5eef68d73f8e58259a72
Rylan Greer <rylangreer@MacBook-Pro.local>
2022-11-24 20:45:29 +0000

    add more docstring for kmeans

--- src/machine_learning_src/k_means.hpp
+++ src/machine_learning_src/k_means.hpp
@@ -38,7 +38,15 @@ public:
         iterations per round.
     */
     KMeans(int n_means, int n_rounds, int n_iterations_per_round);
+
+    /*
+        Fit on a matrix.
+    */
     virtual void fit(const Matrix &X);
+
+    /*
+        Predict cluster labels on a matrix.
+    */
     virtual DoubleVec predict(const Matrix &X) const;
 
 private:
@@ -47,28 +55,54 @@ private:
     int m_n_rounds;
     int m_n_it_per_round;
 
-    // for the fitting algorithm
-    // high-level functions
+    /*
+        For the fitting algorithm -- complete one round
+        and return the labels and the cluster means
+        as a ResultOfRound
+    */
     KMeans_Utils::ResultOfRound complete_one_fitting_round(
         const Matrix &X) const;
 
+    /*
+        Calculate the "score", or average squared distance from cluster mean.
+    */
     double score_from_one_fitting_round(const Matrix &X,
                                         const Matrix &cluster_means,
                                         const std::vector<int> cluster_labels) const;
 
-    // lower level within-round functions
+    /*
+        Initialize the clusters.
+    */
     std::vector<int> initialize_round_clusters(int n_observations) const;
+
+    /*
+        Deprecated. One way of initializing means...
+    */
     Matrix initialize_random_means(const Matrix &X) const;
 
+    /*
+        Given X and labels, calculate the cluster means.
+    */
     Matrix calculate_means_by_cluster(const Matrix &X,
                                       std::vector<int> cluster_labels) const;
 
+    /*
+        Find which cluster label is actually closest to each observation
+        given the current cluster means.
+        Calls find_closest_mean_to_observation for each observation.
+    */
     std::vector<int> reassign_clusters_by_new_cluster_mean(
         const Matrix &X, const Matrix &cluster_means) const;
 
+    /*
+        Find which mean is closest.
+    */
     int find_closest_mean_to_observation(const DoubleVec &observation,
                                          const Matrix &cluster_means) const;
-
+    /*
+        Algorithm currently in use -- start off your cluster means
+        by averaging out real data
+    */
     Matrix initialize_random_means_from_data(const Matrix &X) const;
 
 }; // end of class
