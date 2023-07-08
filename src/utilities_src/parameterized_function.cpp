eaeb68142c44bdf7fbca0178f2e4e27bd0198074
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-08 12:30:56 +0100

    rename params in parameterized functions

--- src/utilities_src/parameterized_function.cpp
+++ src/utilities_src/parameterized_function.cpp
@@ -3,10 +3,10 @@
 ParameterizedFunction::ParameterizedFunction() {}
 
 ParameterizedFunction::ParameterizedFunction(two_vec_func f)
-    : f(f) {}
+    : m_f(f) {}
 
 ParameterizedFunction::ParameterizedFunction(two_vec_func f, DoubleVec W)
-    : f(f), W(W) {}
+    : m_f(f), m_W(W) {}
 
 DoubleVec ParameterizedFunction::operator()(DoubleVec x_)
 {
@@ -15,38 +15,38 @@ DoubleVec ParameterizedFunction::operator()(DoubleVec x_)
 
 void ParameterizedFunction::set_function(two_vec_func f_)
 {
-    this->f = f_;
+    m_f = f_;
 }
 
 void ParameterizedFunction::set_x(DoubleVec x_)
 {
-    this->x = x_;
+    this->m_x = x_;
 }
 
 void ParameterizedFunction::set_W(DoubleVec W_)
 {
-    this->W = W_;
+    this->m_W = W_;
 }
 
 DoubleVec ParameterizedFunction::x_variable_W_constant(DoubleVec x_)
 {
-    return f(x_, W);
+    return m_f(x_, m_W);
 }
 
 DoubleVec ParameterizedFunction::x_constant_W_variable(DoubleVec W_)
 {
-    return f(x, W_);
+    return m_f(m_x, W_);
 }
 
 DoubleVec ParameterizedFunction::x_variable_W_variable(DoubleVec x_,
                                                        DoubleVec W_)
 {
-    return f(x_, W_);
+    return m_f(x_, W_);
 }
 
 double ParameterizedFunction::x_variable_W_constant_1d(DoubleVec x_)
 {
-    DoubleVec z = this->x_variable_W_constant(x_);
+    DoubleVec z = x_variable_W_constant(x_);
     assert(z.size() == 1);
     return z[0];
 }
@@ -54,7 +54,7 @@ double ParameterizedFunction::x_variable_W_constant_1d(DoubleVec x_)
 double ParameterizedFunction::x_variable_W_variable_1d(DoubleVec x_,
                                                        DoubleVec W_)
 {
-    DoubleVec z = this->x_variable_W_variable(x_, W_);
+    DoubleVec z = x_variable_W_variable(x_, W_);
     assert(z.size() == 1);
     return z[0];
 }
