8d861b1713e19a8525066e8cde783664e45cbe43
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-08 14:52:42 +0100

    move some stuff around

--- src/machine_learning_src/lowess.cpp
+++ src/machine_learning_src/lowess.cpp
@@ -5,38 +5,8 @@
 Lowess::Lowess(double p_frac)
 :m_frac(p_frac){}
 
-DoubleVec Lowess::calculate_xx(const DoubleVec& p_x) const
-{
-    return calculate_xy(p_x, p_x);
-}
-
-DoubleVec Lowess::calculate_xy(const DoubleVec& p_x, const DoubleVec& p_y) const
-{
-    DoubleVec xy = p_x * p_y;
-    return xy.rolling_sum(m_window_size, m_offset_for_sum);
-}
-
-void Lowess::save_x_and_y(const Matrix &p_X, const DoubleVec &p_y)
-{
-    m_x = p_X.col(0);
-    m_y = p_y;
-}
-
-void Lowess::fit(const Matrix &p_X, const DoubleVec &p_y)
-{
-    save_x_and_y(p_X, p_y);
-    calculate_coefficients();
-}
-
 DoubleVec Lowess::predict(const Matrix& p_X) const
 {
     DoubleVec ret;
     return ret;
-}
-
-void Lowess::calculate_coefficients()
-{
-    m_xx = calculate_xx(m_x);
-    m_xy = calculate_xy(m_x, m_y);
-
 }
