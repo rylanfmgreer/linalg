c55788eb8c9fc31834916f31e6fda1610ee6a97d
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-08 14:54:22 +0100

    update constness

--- src/machine_learning_src/lowess.hpp
+++ src/machine_learning_src/lowess.hpp
@@ -18,14 +18,18 @@ class Lowess: public SupervisedRegression
     Matrix m_lowess_params;
     DoubleVec m_initial_x_vals;
 
-
+    // helpers for fitting
     DoubleVec calculate_xx_for_fit(const DoubleVec& p_x) const;
     DoubleVec calculate_xy_for_fit(const DoubleVec& p_x, const DoubleVec& p_y) const;
+    void calculate_window_size();
+    void calculate_offset_size();
     void save_x_and_y_for_fit(const Matrix& p_X, const DoubleVec& p_y);
     void calculate_coefficients_for_fit();
 
-    DoubleVec calculate_predictions(const Matrix& p_X);
-    DoubleVec calculate_weights_on_predictions(const Matrix& p_X);
+    // helpers for predicting
+    DoubleVec calculate_raw_linear_estimates(const Matrix& p_X) const;
+    Matrix calculate_weights_for_estimates(const Matrix& p_X) const;
+    DoubleVec calculate_full_predictions(const Matrix& p_X) const;
 
     DoubleVec m_x;
     DoubleVec m_y;
