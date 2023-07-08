0715b4cb6582ad583d9431358c3d681037d67bdf
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-08 11:24:48 +0100

    continued lowess implementation

--- src/machine_learning_src/lowess.cpp
+++ src/machine_learning_src/lowess.cpp
@@ -13,5 +13,25 @@ DoubleVec Lowess::calculate_xx(const DoubleVec& p_x) const
 DoubleVec Lowess::calculate_xy(const DoubleVec& p_x, const DoubleVec& p_y) const
 {
     DoubleVec xy = p_x * p_y;
-    return xy.rolling_sum(window_size, offset_for_sum);
+    return xy.rolling_sum(m_window_size, m_offset_for_sum);
+}
+
+void Lowess::save_x_and_y(const Matrix &p_X, const DoubleVec &p_y)
+{
+    m_x = p_X.col(0);
+    m_y = p_y;
+}
+
+void Lowess::fit(const Matrix &p_X, const DoubleVec &p_y)
+{
+    save_x_and_y(p_X, p_y);
+    calculate_coefficients()
+
+}
+
+void Lowess::calculate_coefficients()
+{
+    m_xx = calculate_xx(m_x);
+    m_xy = calculate_xy(m_x, m_y);
+
 }
--- src/machine_learning_src/lowess.hpp
+++ src/machine_learning_src/lowess.hpp
@@ -9,17 +9,26 @@ class Lowess: public SupervisedRegression
 {
     Lowess(double p_frac);
 
+    public:
+    virtual void fit(const Matrix &X, const DoubleVec &y);
+    virtual DoubleVec predict(const Matrix &X) const;
+
     private:
     double m_frac;
     Matrix m_lowess_params;
     DoubleVec m_initial_x_vals;
 
+
     DoubleVec calculate_xx(const DoubleVec& p_x) const;
     DoubleVec calculate_xy(const DoubleVec& p_x, const DoubleVec& p_y) const;
-    DoubleVec xx;
-    DoubleVec xy;
-    int window_size;
-    int offset_for_sum;
+    void calculate_coefficients();
+
+    DoubleVec m_x;
+    DoubleVec m_y;
+    DoubleVec m_xx;
+    DoubleVec m_xy;
+    int m_window_size;
+    int m_offset_for_sum;
 };
 
 #endif
