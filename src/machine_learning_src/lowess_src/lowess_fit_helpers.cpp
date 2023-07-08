44f5f021b334931c6a3443b604fe72e884930d3f
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-08 19:00:17 +0100

    continue implementation of fit

--- src/machine_learning_src/lowess_src/lowess_fit_helpers.cpp
+++ src/machine_learning_src/lowess_src/lowess_fit_helpers.cpp
@@ -5,13 +5,31 @@ DoubleVec Lowess::calculate_xx_for_fit(const DoubleVec& p_x) const
     return calculate_xy_for_fit(p_x, p_x);
 }
 
+DoubleVec Lowess::calculate_rolling_mean_for_fit(const DoubleVec& p_vec) const
+{
+    return p_vec.rolling_mean(m_window_size, m_offset_for_sum);
+}
 void Lowess::calculate_coefficients_for_fit()
 {
-    m_xx = calculate_xx_for_fit(m_x);
-    m_xy = calculate_xy_for_fit(m_x, m_y);
-
+    m_x_mean = calculate_rolling_mean_for_fit(m_x);
+    m_y_mean = calculate_rolling_mean_for_fit(m_y);
+    m_x_squared = calculate_xx_for_fit(m_x);
+    m_x_times_y = calculate_xy_for_fit(m_x, m_y);
+    m_x_mean = m_x.rolling_mean(m_window_size, m_offset_for_sum);
+    calculate_betas_for_fit();
+    calculate_alphas_for_fit();
 }
 
+void Lowess::calculate_alphas_for_fit()
+{
+    m_alphas = m_y_mean - m_betas * m_x_mean;
+}
+
+void Lowess::calculate_betas_for_fit()
+{
+    m_betas = m_x_times_y / m_x_squared;    
+
+}
 void Lowess::calculate_window_size()
 {
     m_window_size = int(m_x.size() * m_frac);
