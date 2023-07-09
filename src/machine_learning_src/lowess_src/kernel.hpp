f240cf0cecbe892230d3c899db1f500bdde239be
Rylan Greer <rylangreer@MacBook-Pro.local>
2023-07-09 08:09:12 +0100

    remove ref to m fitted weights

--- src/machine_learning_src/lowess_src/kernel.hpp
+++ src/machine_learning_src/lowess_src/kernel.hpp
@@ -1,7 +1,6 @@
 #ifndef _lowess_kernel__9juil2023
-#define _lowess_kernel__9jul2023
+#define _lowess_kernel__9juil2023
 
-#include "../../utilities_src/parameterized_function.hpp
 #include "../lowess.hpp"
 #include "../../matrix_src/matrix.hpp"
 
@@ -15,7 +14,7 @@ class Kernel: public ParameterizedFunction
     private:
     double m_denominator;
     std::function<double (double, double)> m_1d_kernel;
-    DoubleVec& m_fitted_weights;
+    DoubleVec m_fitted_weights;
     Matrix initialize_distance_matrix(const DoubleVec& p_new_x) const;
     Matrix calculate_distances(const DoubleVec& p_new_x) const;
     Matrix apply_weighting_function(Matrix& p_distances) const;
